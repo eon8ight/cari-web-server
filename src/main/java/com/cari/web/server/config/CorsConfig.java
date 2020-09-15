e7fc49508ae45f3d3796ad475b41a234e955d1e6
Chad Miller <chad.miller.3333@gmail.com>
2020-09-14 22:58:33 -0400

    Removed dependncy on dotenv

--- src/main/java/com/cari/web/server/config/CorsConfig.java
+++ src/main/java/com/cari/web/server/config/CorsConfig.java
@@ -4,20 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        Dotenv dotenv = Dotenv.load();
-
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(dotenv.get("CORS_ALLOWED_ORIGINS").split(","));
+                        .allowedOrigins(System.getenv("CARI_CORS_ALLOWED_ORIGINS").split(","));
             }
         };
     }
